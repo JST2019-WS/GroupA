6adeb5e541aa0e247b2920c79b19accc189bc1f6
Liou <liou.bao@tum.de>
2019-09-30 19:09:31 +0200

    added db Information

--- src/mongodb.js
+++ src/mongodb.js
@@ -1,17 +1,16 @@
-const parse = require('mongodb-core').parseConnectionString;
 const MongoClient = require('mongodb').MongoClient;
 const logger = require('./logger');
 
 module.exports = function (app) {
   const config = app.get('mongodb');
-  const promise = MongoClient.connect(config, { useNewUrlParser: true }).then(client => {
+  const promise = MongoClient.connect(config.url, { useNewUrlParser: true, useUnifiedTopology: true}).then(client => {
     // For mongodb <= 2.2
     if(client.collection) {
       return client;
     }
 
-    const dbName = parse(config, () => {});
-    return client.db(dbName);
+    console.log('Database connected to ' + config.dbname);
+    return client.db(config.dbname);
   }).catch(error => {
     logger.error(error);
   });
