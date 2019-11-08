d083dde5228af4f1351c5fb1b9f3798621e22b9c
Ludwig Dickmanns <dickmanns@fortiss.org>
2019-11-08 17:11:03 +0100

    Recommendation: Risk indicators.
    * Implemented risk indicator based on security type in combination with 6 risk classes derived from WpHG questionnaires.

--- src/recommendation/risk-indicators/security-type-ri.js
+++ src/recommendation/risk-indicators/security-type-ri.js
@@ -1,7 +1,79 @@
+const csv = require('csvtojson');
+
 // Risk percentage calculations are only based on the isin and should return a value between 0.0 and 100.0
 module.exports = {
-  riskPercentage : (isin) => {
-    // TODO add calculations here
-    return -1.0;
+  riskPercentage : async (isin) => {
+    let sec = null;
+    const securities = await csv().fromFile('../data/all-securities.csv');
+    for (let security of securities) {
+      if (security.isin === isin) {
+        sec = security;
+        break;
+      }
+    }
+
+    if (sec === null) return null;
+
+    let riskClass = null;
+    let type = sec.name;
+
+    switch (type) {
+      case 'STK':
+        riskClass = 4;
+        break;
+      case 'FND':
+        riskClass = 4;
+        break;
+      case 'IND':
+        riskClass = -1;
+        break;
+      case 'ETF':
+        riskClass = 4;
+        break;
+      case 'KNO':
+        riskClass = 5;
+        break;
+      case 'ZER':
+        riskClass = 4;
+        break;
+      case 'CUR':
+        riskClass = -1;
+        break;
+      case 'ETC':
+        riskClass = 1;
+        break;
+      case 'BND':
+        riskClass = 1;
+        break;
+      case 'CRYP':
+        riskClass = 5;
+        break;
+      case 'ETN':
+        riskClass = 5;
+        break;
+      case 'FUT':
+        riskClass = 5;
+        break;
+      case 'INT':
+        riskClass = -1;
+        break;
+      case 'OPT':
+        riskClass = 5;
+        break;
+      case 'RES':
+        riskClass = 3;
+        break;
+      case 'WNT':
+        riskClass = 5;
+        break;
+      default:
+        console.log('Unmatched Security Type!');
+        return null;
+    }
+
+    if (riskClass === -1) return null;
+
+    return riskClass * 16.667 + 16.667 / 2;
   }
 };
+
