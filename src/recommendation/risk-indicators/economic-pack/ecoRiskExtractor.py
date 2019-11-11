ff8db6286e80f5fdb68525789ad2e5287931ccc4
Ashish <ashish.khanal@hotmail.com>
2019-11-11 02:29:34 +0100

    creadted economic pack

---
+++ src/recommendation/risk-indicators/economic-pack/ecoRiskExtractor.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+import sys
+import pycountry
+import csv
+
+# convert the ISIN into a ticker symbol
+isin = sys.argv[1]
+
+# Hard coded values for missing ISINs
+special_codes = {
+    'AN':'Netherlands Antilles',
+    'CS':'Serbia',
+    'EU':'European Union',
+    'ZR':'Congo'
+}
+
+# Prevent different country name clashes for the same country
+country_name_new = {
+    'Czechia':'Czech Republic',
+    'Russian Federation':'Russia',
+    'Venezuela, Bolivarian Republic of':'Venezuela',
+    'Brunei Darussalam':'Brunei',
+    'Côte d\'Ivoire':'Ivory Coast'
+}
+
+
+# Converting ISINs to country
+country_name = ''
+country_code = isin[:2]
+try:
+    country = pycountry.countries.get(alpha_2=country_code.upper())
+    try:
+        country_name = country_name_new[country.name]
+    except:
+        country_name = country.name
+except:
+    try:
+        country = special_codes[country_code]
+        country_name = country
+    except:
+        country_name = 'NULL'
+
+# Retrieving the ratings for the countries from csv file
+# Scaling the rating [0 - 100], -100 being invalid
+credit_ratings = {}
+with open("countryRating.csv") as ctrRating:
+    ratings = csv.reader(ctrRating)
+    for count, row in enumerate(ratings):
+        if count > 0:
+            credit_ratings[row[0]] = row[5]
+
+rating = -100
+try:
+    rating = credit_ratings[country_name]
+except:
+    pass
+
+print(rating)
---
+++ src/recommendation/risk-indicators/economic-pack/economic-ri.js
@@ -0,0 +1,20 @@
+const { exec } = require('child_process');
+
+// Risk percentage calculations are only based on the isin and should return a value between 0.0 and 100.0
+module.exports = {
+    riskPercentage(isin, callback) {
+      var result = null;
+      exec('python ecoRiskExtractor.py '+isin, function(err, stdout, stderr){
+          if(err){
+              console.log(err.stack);
+          }
+          var beta = parseFloat(stdout);
+          if(beta != -100){
+              result = beta;
+          }
+          //return the value by invoking a callback
+          //the 1st param. is error, just keep it null
+          callback(null, result);
+      });
+    }
+};
