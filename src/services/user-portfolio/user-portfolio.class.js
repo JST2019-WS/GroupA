9c3e8c6ef5729783c04c6530abc9f886049727a1
lee <liou@gmx.at>
2019-11-18 19:23:28 +0100

    update SecurityKey

--- src/services/user-portfolio/user-portfolio.class.js
+++ src/services/user-portfolio/user-portfolio.class.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-unused-vars */
+const {NotFound, GeneralError, BadRequest} = require('@feathersjs/errors');
 const handleRequest = require('./hooks/handleRequest');
 
 class Service {
@@ -11,6 +12,50 @@ class Service {
       return Promise.all(data.map(current => this.create(current)));
     }
     return handleRequest(data, params);
+  }
+
+  async patch(id, data, params) {
+    const app = require('../../app');
+    const record = {'service': 'userPortfolio', 'action': 'updateSecurityKey'};
+    const saveMonitoringRecord = require('../../helper/saveMonitoringRecord');
+
+    if (data.oldKey === undefined) {
+      saveMonitoringRecord.saveRecord(record, false, 'Missing oldKey field!');
+      return new BadRequest('Missing oldKey field!');
+    }
+    if (data.oldKey !== process.env.WSO_SECURITY_KEY) {
+      saveMonitoringRecord.saveRecord(record, false, 'Wrong oldKey!');
+      return new BadRequest('Wrong oldKey!');
+    }
+    if (data.newKey === undefined) {
+      saveMonitoringRecord.saveRecord(record, false, 'Missing newKey field!');
+      return new BadRequest('Missing newKey field!');
+    }
+
+    let success = false;
+    let fs = require('fs');
+    fs.readFile('.env', 'utf8', function (err, fileData) {
+      success = err;
+      if (err) {
+        console.log(err);
+      } else {
+        let result = fileData.replace('WSO_SECURITY_KEY=\'' + data.oldKey + '\'', 'WSO_SECURITY_KEY=\'' + data.newKey + '\'');
+        fs.writeFile('.env', result, 'utf8', function (err) {
+          process.env.WSO_SECURITY_KEY = data.newKey;
+          success = err;
+          if (err) return console.log(err);
+        });
+      }
+    });
+
+    if(success){
+      saveMonitoringRecord.saveRecord(record, false, 'An Error occurred while saving the new Key!');
+      return new BadRequest('An Error occurred while saving the new Key!');
+    }
+    else{
+      saveMonitoringRecord.saveRecord(record, true, 'Updated securityKey!');
+      return Promise.resolve('Updated securityKey!');
+    }
   }
 }
 
